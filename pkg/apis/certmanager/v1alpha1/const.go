fcdf14f9aec34cd2ee8c2774fa880050e0cc8a0a
Brian Lai <brian.lai.ym@gmail.com>
2018-11-13 14:03:47 -0800

    Reduce cert duration limit for testing

--- pkg/apis/certmanager/v1alpha1/const.go
+++ pkg/apis/certmanager/v1alpha1/const.go
@@ -20,13 +20,13 @@ import "time"
 
 const (
 	// minimum permitted certificate duration by cert-manager
-	MinimumCertificateDuration = time.Hour
+	MinimumCertificateDuration = time.Minute
 
 	// default certificate duration if Issuer.spec.duration is not set
 	DefaultCertificateDuration = time.Hour * 24 * 90
 
 	// minimum certificate duration before certificate expiration
-	MinimumRenewBefore = time.Minute * 5
+	MinimumRenewBefore = time.Second * 30
 
 	// Default duration before certificate expiration if  Issuer.spec.renewBefore is not set
 	DefaultRenewBefore = time.Hour * 24 * 30
